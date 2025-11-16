f3d8565b87b5e03ef2f235babcf741ef991c0611
Antonio Ibarra <ai2335102@gmail.com>
2025-11-16 13:51:20 -0800

    Adicion del campo 'precio' en 'Products

--- main/models.py
+++ main/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     mult2 = models.IntegerField(default=0)
     unidad_medida1 = models.ForeignKey('UnidadMedida', null=True, blank=True, on_delete=models.SET_NULL, related_name='products_unidad1')
     unidad_medida2 = models.ForeignKey('UnidadMedida', null=True, blank=True, on_delete=models.SET_NULL, related_name='products_unidad2')
+    precio = models.DecimalField(max_digits=12, decimal_places=4, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
