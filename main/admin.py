5d5ce00dbebfd6fc005531e7f0bd2e7bf359b620
Antonio Ibarra <ai2335102@gmail.com>
2026-01-07 11:50:53 -0800

    Add ango-import-export library

--- main/admin.py
+++ main/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import Cliente, UnidadMedida, Pedido, Categoria
 
 
 @admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
+class ClienteAdmin(admin.ModelAdmin, ImportExportModelAdmin):
 	list_display = ('id_cliente', 'descripcion')
 
 
 @admin.register(UnidadMedida)
-class UnidadMedidaAdmin(admin.ModelAdmin):
+class UnidadMedidaAdmin(admin.ModelAdmin, ImportExportModelAdmin):
 	list_display = ('id', 'descripcion')
 
 @admin.register(Pedido)
-class PedidoAdmin(admin.ModelAdmin):
+class PedidoAdmin(admin.ModelAdmin, ImportExportModelAdmin):
 	list_display = ('id_pedido', 'cliente', 'productos', 'estatus','created_at', 'total')
 	list_filter = ('estatus', 'cliente')
 	search_fields = ('folio', 'productos')
 
 @admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaAdmin(admin.ModelAdmin, ImportExportModelAdmin):
 	list_display = ('id', 'description')
